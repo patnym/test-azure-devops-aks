fdab4c831f30c1c56b65c25b53560ffbd79b8626
patnym <patrik.nyman@axakon.se>
2020-01-15 05:27:50 +0100

    Print env

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send(JSON.stringify(process.env));
 });
 
 app.listen(PORT, HOST);
